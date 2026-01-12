a17c45fdd55115e48d11e4586d529b1d720ca4c1
tracy.ma <tracy.ma@simpletruss.com>
2026-01-12 17:32:47 +0800

    modify cache for multi tenant

--- src/Plugins/BotSharp.Plugin.MultiTenancy/MultiTenancy/DbTenantStore.cs
+++ src/Plugins/BotSharp.Plugin.MultiTenancy/MultiTenancy/DbTenantStore.cs
@@ -1,6 +1,5 @@
 using BotSharp.Abstraction.MultiTenancy;
 using BotSharp.Abstraction.MultiTenancy.Options;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,30 +8,20 @@ namespace BotSharp.Plugin.MultiTenancy.MultiTenancy;
 
 public class DbTenantStore : ITenantStore
 {
-    private const string CacheKey = "tenant_store:tenants";
-
     private readonly ITenantRepository _repo;
-    private readonly IMemoryCache _cache;
     private readonly ILogger<DbTenantStore> _logger;
 
-    public DbTenantStore(ITenantRepository repo, IMemoryCache cache, ILogger<DbTenantStore> logger)
+    public DbTenantStore(ITenantRepository repo, ILogger<DbTenantStore> logger)
     {
         _repo = repo;
-        _cache = cache;
         _logger = logger;
     }
 
     public List<TenantConfiguration> GetTenants()
     {
-        if (_cache.TryGetValue(CacheKey, out List<TenantConfiguration> cached))
-        {
-            return cached;
-        }
-
         try
         {
             var tenants = _repo.GetTenants();
-            _cache.Set(CacheKey, tenants, TimeSpan.FromMinutes(5));
             return tenants;
         }
         catch (Exception ex)
